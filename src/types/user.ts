118486a2c6584eb3bba113a1ef148c8afe493f3d
nanaezzze-star <nanaezzze@gmail.com>
2026-09-17 15:32:16 +0300

    remove legacy files

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  role: "employee" | "author" | "admin";
-  position: string;
-  avatarUrl?: string;
-}
